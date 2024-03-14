576d65b852e169a2af976767c095442e370c6adb
vathsalyakidambi <vathsalyakidambi@pingidentity.com>
2024-03-14 16:58:38 -0500

    [skip pipeline]

--- code-gen/profiles/pingdirectory/instance/lib/start_server_test.sh
+++ code-gen/profiles/pingdirectory/instance/lib/start_server_test.sh
@@ -3,4 +3,7 @@
 # No-op to prevent errors due to "publishNotReadyAddresses" not being set to "true" on the pingdirectory service.
 # This field is not required in P1AS because we enable replication in offline mode and don't use any of the
 # topology-related tools such as dsreplication and remove-defunct-server.
-exit 0
+echo "test PDO-6682"
+while true; do
+   sleep 60
+done
